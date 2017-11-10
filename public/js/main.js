adb65c48fc67c0b8d05f40a0f4b2079423b62c69
Tzirath <tzirath.perez@hotmail.com>
2017-11-10 17:16:58 +0000

    little bit change

--- public/js/main.js
+++ public/js/main.js
@@ -9,3 +9,13 @@ function myFunction() {
   $("#doge-image").append(`<img class="img-circle" src="/images/wowdoge.jpeg" />`);
 }
 
+function myFunction1() {
+  // use jQuery ($ is shorthand) to find the div on the page and then change the html
+  // jQuery can do a lot of crazy stuff so make sure to google around to find out more
+
+  $("#demo").html("NEWWW PARAGRAPH #javascript #fire");
+
+  // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
+  $("#beach").append(`<img class="img-circle" src="/images/IMG_0203" />`);
+}
+
